b681c33c700bc7452a2a7f2f870d3dee36f9f912
Rune Flobakk <flobakk@gmail.com>
2014-05-13 15:04:16 +0200

    validate.sh avslutter med non-zero exitcode ved valideringsfeil, for å kunne brukes til automatisk validering f.eks. på byggserver.

--- eksempler/validate.sh
+++ eksempler/validate.sh
@@ -3,6 +3,25 @@
 eksempler=$(dirname "$0")
 xsds="${eksempler}/../xsd"
 
-xmllint --noout --schema ${xsds}/sdp.xsd ${eksempler}/sdp*.xml
-xmllint --noout --schema ${xsds}/SBDH20040506-02/StandardBusinessDocumentHeader.xsd ${eksempler}/sbdh.xml
+
+function validate {
+  local xmlFile="$1"
+  local schema="$2"
+  xmllint --noout --schema ${schema} ${xmlFile}
+}
+
+errors=0
+for eksempel in ${eksempler}/sdp*.xml; do
+  validate ${eksempel} ${xsds}/sdp.xsd || ((errors++))
+done
+
+for eksempel in ${eksempler}/sbdh.xml; do
+  validate ${eksempel} ${xsds}/SBDH20040506-02/StandardBusinessDocumentHeader.xsd || ((errors++))
+done
+
+
+if (( errors > 0 )); then
+  echo Fant ${errors} filer med valideringsfeil!
+  exit 1
+fi
 
